7448157e8cfd4e1425e771e0b7dfd069d9637ddc
Vinicius Santos <vinicius.santanasantos@yahoo.com.br>
2022-03-15 18:15:16 -0300

    correções feitas pelo eslint/prettier

--- ufabc_planner-backend/src/server.ts
+++ ufabc_planner-backend/src/server.ts
@@ -1,14 +1,14 @@
-import express from "express"
-import { PrismaClient } from "@prisma/client"
+import express from 'express';
+import { PrismaClient } from '@prisma/client';
 
-const app = express()
+const app = express();
 
-const prisma = new PrismaClient()
+const prisma = new PrismaClient();
 
 app.use(express.json());
 
 app.get('/', (req, res) => {
-  res.json('Hello World')
-})
+  res.json('Hello World');
+});
 
-app.listen(3333, () => console.log("Server is up and running"));
+app.listen(3333, () => console.log('Server is up and running'));
