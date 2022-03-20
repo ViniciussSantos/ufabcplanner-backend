47200cbdad90c17d97d3041795ca1721bb90cd47
Vinicius Santos <vinicius.santanasantos@yahoo.com.br>
2022-03-20 18:58:35 -0300

    dividi lógica da api entre app.ts e server.ts

---
+++ ufabc_planner-backend/src/infra/app.ts
@@ -0,0 +1,26 @@
+import 'reflect-metadata';
+import cors from 'cors';
+import express, { NextFunction, Request, Response } from 'express';
+import 'express-async-errors';
+import { AppError } from 'utils/errors/AppError';
+import { router } from './routes';
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+app.use(router);
+
+app.use((err: Error, request: Request, response: Response, next: NextFunction) => {
+  if (err instanceof AppError) {
+    return response.status(400).json({
+      message: err.message,
+    });
+  }
+  return response.status(500).json({
+    status: 'error',
+    message: 'Internal server error',
+  });
+});
+
+export { app };
