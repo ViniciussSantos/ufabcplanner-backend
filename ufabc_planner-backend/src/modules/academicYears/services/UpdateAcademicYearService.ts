6adf965f644a2dd934cb7cc55f56ad30e822ae0e
Vinicius Santos <vinicius.santanasantos@yahoo.com.br>
2022-04-10 15:49:24 -0300

    melhorei lógica de update de ano acadêmico

--- ufabc_planner-backend/src/modules/academicYears/services/UpdateAcademicYearService.ts
+++ ufabc_planner-backend/src/modules/academicYears/services/UpdateAcademicYearService.ts
@@ -1,44 +1,53 @@
+import { AcademicYear } from '@prisma/client';
 import { DateProvider } from 'providers/dateProvider';
 import { injectable } from 'tsyringe';
 import { AppError } from 'utils/errors/AppError';
 import { prisma } from 'utils/prisma';
+import { UpdateAcademyYearDTO } from '../dtos/UpdateAcademicYearDTO';
 
 @injectable()
 export class UpdateAcademicYearService {
   constructor(private readonly dateProvider: DateProvider) {}
 
-  async execute(academicYearId: string, params: UpdateAcademyYearDTO): Promise<void> {
-    const academicYearExists = await prisma.academicYear.findUnique({ where: { id: academicYearId } });
+  checkDates(originalAcademicYear: AcademicYear, validParams: Omit<UpdateAcademyYearDTO, 'id'>): void {
+    if (validParams.start_date) {
+      if (
+        this.dateProvider.compareIfBefore(
+          this.dateProvider.toDate(validParams.start_date),
+          originalAcademicYear.end_date
+        )
+      ) {
+        throw new AppError('Data final é antes da data inicial');
+      }
+    }
 
-    if (!academicYearExists) {
+    if (validParams.end_date) {
+      if (
+        this.dateProvider.compareIfBefore(
+          originalAcademicYear.start_date,
+          this.dateProvider.toDate(validParams.end_date)
+        )
+      ) {
+        throw new AppError('Data final é antes da data inicial');
+      }
+    }
+  }
+
+  async execute(params: UpdateAcademyYearDTO): Promise<void> {
+    const originalAcademicYear = await prisma.academicYear.findUnique({ where: { id: params.id } });
+
+    if (!originalAcademicYear) {
       throw new AppError('Este ano acadêmico não existe!');
     }
 
-    const academicYearInput = {
-      year: params.year,
-      start_date: params.start_date ? this.dateProvider.toDate(params.start_date) : undefined,
-      end_date: params.end_date ? this.dateProvider.toDate(params.end_date) : undefined,
-    };
-
-    const academicYearToSave: any = {};
-
-    Object.entries(academicYearInput).map(([key, value]) => {
-      if (value !== undefined) {
-        academicYearToSave[key] = value;
-      }
-    });
-
-    console.log(Object.keys(academicYearToSave));
-    console.log(params);
-
-    if (Object.keys(academicYearToSave).length === 0) {
-      throw new AppError('Necessário pelo menos um campo para atualizar');
-    }
+    this.checkDates(originalAcademicYear, params);
 
     await prisma.academicYear.update({
-      where: { id: academicYearId },
+      where: { id: params.id },
       data: {
-        ...academicYearToSave,
+        year: params.year,
+        start_date: params.start_date,
+        end_date: params.end_date,
       },
     });
   }
