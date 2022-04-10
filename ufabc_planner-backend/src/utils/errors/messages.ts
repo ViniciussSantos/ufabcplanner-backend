4c2f36f0a86fce6c9d5d0d801877cab54354739c
Vinicius Santos <vinicius.santanasantos@yahoo.com.br>
2022-04-10 14:13:25 -0300

    mensagem de erro para os DTOs

---
+++ ufabc_planner-backend/src/utils/errors/messages.ts
@@ -0,0 +1,6 @@
+export const messages = {
+  IsUUID: '$property deve ser um uuid válido',
+  isNotEmpty: '$property não pode ser vazio',
+  IsNumberString: '$property deve ser uma string de número',
+  IsDateString: '$property deve ser uma string de data válida',
+};
