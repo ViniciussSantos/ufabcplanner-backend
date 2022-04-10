31a1bfc56f46ec54a51ad2b632a8d8d677cdaecc
Vinicius Santos <vinicius.santanasantos@yahoo.com.br>
2022-04-10 15:47:10 -0300

    Dtos com validação

--- ufabc_planner-backend/src/modules/academicYears/dtos/UpdateAcademicYearDTO.ts
+++ ufabc_planner-backend/src/modules/academicYears/dtos/UpdateAcademicYearDTO.ts
@@ -1,5 +1,20 @@
-interface UpdateAcademyYearDTO {
-  year: string | undefined;
-  start_date: string | undefined;
-  end_date: string | undefined;
+import { IsDateString, IsNotEmpty, IsNumberString, IsOptional, IsUUID } from 'class-validator';
+import { messages } from 'utils/errors/messages';
+import { IUpdateAcademyYear } from './interfaces/IUpdateAcademicYear';
+
+export class UpdateAcademyYearDTO implements IUpdateAcademyYear {
+  @IsUUID('all', { message: messages.IsUUID })
+  @IsNotEmpty({ message: messages.isNotEmpty })
+  id: string;
+
+  @IsNumberString({ message: messages.IsNumberString })
+  year: string;
+
+  @IsOptional()
+  @IsDateString({ message: messages.IsDateString })
+  start_date?: string;
+
+  @IsOptional()
+  @IsDateString({ message: messages.IsDateString })
+  end_date?: string;
 }
--- ufabc_planner-backend/src/modules/users/dtos/AuthenticateUserDTO.ts
+++ ufabc_planner-backend/src/modules/users/dtos/AuthenticateUserDTO.ts
@@ -1,10 +1,14 @@
-interface AuthenticateUserDTO {
+import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
+import { messages } from 'utils/errors/messages';
+import { IAuthenticateUser } from './interfaces/IAuthenticateUser';
+
+export class AuthenticateUserDTO implements IAuthenticateUser {
+  @IsNotEmpty({ message: messages.isNotEmpty })
+  @IsString()
+  @IsEmail()
   email: string;
+
+  @IsNotEmpty({ message: messages.isNotEmpty })
+  @IsString()
   password: string;
 }
-
-interface AuthenticateUserResponseDTO {
-  token: string;
-}
-
-export { AuthenticateUserDTO, AuthenticateUserResponseDTO };
