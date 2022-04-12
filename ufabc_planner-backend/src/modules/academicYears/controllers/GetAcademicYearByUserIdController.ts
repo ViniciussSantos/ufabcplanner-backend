2b5e14a8931bcf411be0ffb90e23f611177a6902
Vinicius Santos <vinicius.santanasantos@yahoo.com.br>
2022-04-12 17:13:31 -0300

    controller para GET do ano acadêmico a partir do id do usuário

---
+++ ufabc_planner-backend/src/modules/academicYears/controllers/GetAcademicYearByUserIdController.ts
@@ -0,0 +1,15 @@
+import { Request, Response } from 'express';
+import { container } from 'tsyringe';
+import { GetAcademicYearByUserIdService } from '../services/GetAcademicYearByUserIdService';
+
+export class GetAcademicYearByUserIdController {
+  async handle(request: Request, response: Response) {
+    const { id: userId } = request.user;
+
+    const getAcademicYearByUserIdService = container.resolve(GetAcademicYearByUserIdService);
+
+    const academicYears = await getAcademicYearByUserIdService.execute(userId);
+
+    return response.json(academicYears).send();
+  }
+}
