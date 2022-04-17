cc57e295e9390cc2700efe283a1491759e69a536
Vinicius Santos <vinicius.santanasantos@yahoo.com.br>
2022-04-17 20:58:20 -0300

    atualização dos imports

--- ufabc_planner-backend/src/modules/academicYears/controllers/UpdateAcademicYearController.ts
+++ ufabc_planner-backend/src/modules/academicYears/controllers/UpdateAcademicYearController.ts
@@ -1,6 +1,6 @@
 import { Request, Response } from 'express';
 import { container } from 'tsyringe';
-import { validateInput } from 'utils/errors/validation';
+import { validateInput } from 'infra/http/errors/validation';
 import { UpdateAcademyYearDTO } from '../dtos/UpdateAcademicYearDTO';
 import { UpdateAcademicYearService } from '../services/UpdateAcademicYearService';
 
