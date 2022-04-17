82f3b35795994ec91f25bc2a26a699f46539fd23
Vinicius Santos <vinicius.santanasantos@yahoo.com.br>
2022-04-17 20:46:55 -0300

    atualizando imports

--- ufabc_planner-backend/src/modules/academicYears/dtos/CreateAcademyYearDTO.ts
+++ ufabc_planner-backend/src/modules/academicYears/dtos/CreateAcademyYearDTO.ts
@@ -1,11 +1,11 @@
 import { IsDateString, IsNotEmpty, IsNumberString, IsUUID } from 'class-validator';
-import { messages } from 'utils/errors/messages';
+import { messages } from 'infra/http/errors/messages';
 import { ICreateAcademyYear } from './interfaces/ICreateAcademyYear';
 
 export class CreateAcademyYearDTO implements ICreateAcademyYear {
   @IsUUID('all', { message: messages.IsUUID })
   @IsNotEmpty({ message: messages.isNotEmpty })
-  id: string;
+  userId: string;
 
   @IsNotEmpty({ message: messages.isNotEmpty })
   @IsNumberString({ message: messages.IsNumberString })
