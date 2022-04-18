d4df4abdb90cf079f398190fca026b0e644b0a39
Vinicius Santos <vinicius.santanasantos@yahoo.com.br>
2022-04-18 20:48:19 -0300

    teste para criação de usuários

---
+++ ufabc_planner-backend/src/modules/accounts/controllers/CreateUserController.spec.ts
@@ -0,0 +1,27 @@
+import { app } from 'infra/http/app';
+import supertest from 'supertest';
+import { deleteAll, disconnect } from '../../../../test/database';
+import { getUserByEmail } from '../../../../test/entities/UserFactory';
+
+describe('Create User (e2e)', () => {
+  beforeEach(async () => {
+    deleteAll();
+  });
+
+  afterAll(async () => {
+    disconnect();
+  });
+  it('should register user correctly', async () => {
+    const response = await supertest(app).post('/users/').send({
+      name: 'teste',
+      email: 'teste@email.com',
+      password: '123',
+    });
+
+    expect(response.status).toBe(201);
+
+    const user = await getUserByEmail('teste@email.com');
+
+    expect(user).toBeTruthy();
+  });
+});
