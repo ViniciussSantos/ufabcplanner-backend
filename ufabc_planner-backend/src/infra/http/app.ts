c394b2dbfc12b9854e9d4bb4eef14e0c01e58cd4
Vinicius Santos <vinicius.santanasantos@yahoo.com.br>
2022-04-18 20:50:53 -0300

    atualizei import do index do container

--- ufabc_planner-backend/src/infra/http/app.ts
+++ ufabc_planner-backend/src/infra/http/app.ts
@@ -6,7 +6,8 @@ import { AppError } from 'infra/http/errors/AppError';
 import { router } from './routes';
 import swaggerUi from 'swagger-ui-express';
 import swaggerFile from 'swagger.json';
-import '/infra/container';
+import 'infra/container';
+
 const app = express();
 
 app.use(express.json());
