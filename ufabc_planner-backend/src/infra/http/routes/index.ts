bdc21dc6985f38567b76dc10b5b389d1d5e29563
Vinicius Santos <vinicius.santanasantos@yahoo.com.br>
2022-04-21 21:54:06 -0300

    rotas de quadrimestres

--- ufabc_planner-backend/src/infra/http/routes/index.ts
+++ ufabc_planner-backend/src/infra/http/routes/index.ts
@@ -1,10 +1,12 @@
 import { Router } from 'express';
 import { academicYearRoutes } from './academicYears.routes';
+import { quartersRoutes } from './quarters.routes';
 import { usersRoutes } from './users.routes';
 
 const router = Router();
 
 router.use('/users', usersRoutes);
 router.use('/academicyears', academicYearRoutes);
+router.use('/quarters', quartersRoutes);
 
 export { router };
