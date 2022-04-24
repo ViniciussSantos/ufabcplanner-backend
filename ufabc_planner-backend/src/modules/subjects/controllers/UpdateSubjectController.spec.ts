774e4e83b32fa53ade91ca0fa07269b36f8505fc
Vinicius Santos <vinicius.santanasantos@yahoo.com.br>
2022-04-24 00:21:20 -0300

    rota para atualizar matérias

---
+++ ufabc_planner-backend/src/modules/subjects/controllers/UpdateSubjectController.spec.ts
@@ -0,0 +1,35 @@
+import { app } from 'infra/http/app';
+import supertest from 'supertest';
+import { deleteAll, disconnect } from '../../../../test/database';
+import { CreateAcademicYear } from '../../../../test/entities/AcademicYearFactory';
+import { createQuarter } from '../../../../test/entities/QuarterFactory';
+import { createSubject } from '../../../../test/entities/SubjectFactory';
+import { createUser, authenticateUser } from '../../../../test/entities/UserFactory';
+
+describe('update subject (e2e)', () => {
+  beforeAll(async () => {
+    deleteAll();
+  });
+
+  afterAll(async () => {
+    disconnect();
+  });
+
+  it("should update the subject's name and description", async () => {
+    const user = await createUser();
+    const token = await authenticateUser(user);
+    const academicYear = await CreateAcademicYear(user);
+    const quarter = await createQuarter(academicYear);
+    const subject = await createSubject(quarter);
+
+    const response = await supertest(app)
+      .put('/subjects/update/' + subject.id)
+      .set('authorization', 'Bearer ' + token)
+      .send({
+        name: 'Paradigmas da programação',
+        description: 'descrição teste',
+      });
+
+    expect(response.status).toBe(204);
+  });
+});
