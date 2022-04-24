1f6d9d40a11e3e49a4a1cad9be6e2d47e735e18e
Vinicius Santos <vinicius.santanasantos@yahoo.com.br>
2022-04-24 00:11:24 -0300

    nova mensagem de erro

--- ufabc_planner-backend/src/infra/http/errors/messages.ts
+++ ufabc_planner-backend/src/infra/http/errors/messages.ts
@@ -1,4 +1,5 @@
 export const messages = {
+  IsString: '$property deve ser uma string válida',
   IsUUID: '$property deve ser um uuid válido',
   isNotEmpty: '$property não pode ser vazio',
   IsNumberString: '$property deve ser uma string de número',
