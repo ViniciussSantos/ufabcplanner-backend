76bd9992aec24c675844ddb6d72918c23578568b
Vinicius Santos <vinicius.santanasantos@yahoo.com.br>
2022-05-10 03:36:53 -0300

    testes para as rotas de tasks

---
+++ ufabc_planner-backend/src/modules/tasks/controllers/GetTasksBySubjectId.spec.ts
@@ -0,0 +1,36 @@
+import { app } from 'infra/http/app';
+import supertest from 'supertest';
+import { deleteAll, disconnect } from '../../../../test/database';
+import { createAcademicYear } from '../../../../test/entities/AcademicYearFactory';
+import { createQuarter } from '../../../../test/entities/QuarterFactory';
+import { createSubject } from '../../../../test/entities/SubjectFactory';
+import { createTask } from '../../../../test/entities/TaskFactory';
+import { createUser, authenticateUser } from '../../../../test/entities/UserFactory';
+
+describe('Get tasks by subject Id (e2e)', () => {
+  beforeAll(async () => {
+    deleteAll();
+  });
+
+  afterAll(async () => {
+    disconnect();
+  });
+
+  it('Should return a task successfully using the subject id', async () => {
+    const user = await createUser();
+    const token = await authenticateUser(user);
+    const academicYear = await createAcademicYear(user);
+    const quarter = await createQuarter(academicYear);
+    const subject = await createSubject(quarter);
+    const task = await createTask(subject, user);
+
+    const response = await supertest(app)
+      .get('/tasks/get/subject/' + subject.id)
+      .set('authorization', 'Bearer ' + token);
+
+    const responseBody = JSON.parse(response.text);
+
+    expect(response.status).toBe(200);
+    expect(responseBody.length).toBe(1);
+  });
+});
