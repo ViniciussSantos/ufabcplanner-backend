a1a648cca1ba6d30af1dffcdcb4776c62a3cced4
Vinicius Santos <vinicius.santanasantos@yahoo.com.br>
2022-05-10 22:27:41 -0300

    atualização da transaction de limpeza do banco

--- ufabc_planner-backend/test/database.ts
+++ ufabc_planner-backend/test/database.ts
@@ -6,7 +6,17 @@ export async function deleteAll() {
   const deleteQuarter = prisma.quarter.deleteMany();
   const deleteSubject = prisma.subject.deleteMany();
   const deleteClass = prisma.class.deleteMany();
-  await prisma.$transaction([deleteClass, deleteSubject, deleteQuarter, deleteAcademicYear, deleteUser]);
+  const deleteTask = prisma.task.deleteMany();
+  const deleteExam = prisma.exam.deleteMany();
+  await prisma.$transaction([
+    deleteExam,
+    deleteTask,
+    deleteClass,
+    deleteSubject,
+    deleteQuarter,
+    deleteAcademicYear,
+    deleteUser,
+  ]);
 }
 
 export async function disconnect() {
