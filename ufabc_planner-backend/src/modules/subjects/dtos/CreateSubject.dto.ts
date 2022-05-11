3945e679b0a267c85338da52cd6a83f49e678a6e
Vinicius Santos <vinicius.santanasantos@yahoo.com.br>
2022-05-10 23:13:41 -0300

    atualizaçao do dto de createSubject

--- ufabc_planner-backend/src/modules/subjects/dtos/CreateSubject.dto.ts
+++ ufabc_planner-backend/src/modules/subjects/dtos/CreateSubject.dto.ts
@@ -3,6 +3,14 @@ import { messages } from 'infra/http/errors/messages';
 import { ICreateSubject } from './interfaces/ICreateSubject';
 
 export class CreateSubjectDTO implements ICreateSubject {
+  @IsUUID('all', { message: messages.IsUUID })
+  @IsNotEmpty({ message: messages.isNotEmpty })
+  quarterId: string;
+
+  @IsUUID('all', { message: messages.IsUUID })
+  @IsNotEmpty({ message: messages.isNotEmpty })
+  userId: string;
+
   @IsString({ message: messages.IsString })
   @IsNotEmpty({ message: messages.isNotEmpty })
   name: string;
@@ -10,8 +18,4 @@ export class CreateSubjectDTO implements ICreateSubject {
   @IsOptional()
   @IsString({ message: messages.IsString })
   description?: string;
-
-  @IsUUID('all', { message: messages.IsUUID })
-  @IsNotEmpty({ message: messages.isNotEmpty })
-  quarterId: string;
 }
