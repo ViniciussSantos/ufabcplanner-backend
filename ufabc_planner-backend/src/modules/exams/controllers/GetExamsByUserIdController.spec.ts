45d3dc6b41dcc9fe287a32a71cee60dcd9827434
Vinicius Santos <vinicius.santanasantos@yahoo.com.br>
2022-05-10 22:27:13 -0300

    testes para as rotas de exams

---
+++ ufabc_planner-backend/src/modules/exams/controllers/GetExamsByUserIdController.spec.ts
@@ -0,0 +1,36 @@
+import { app } from 'infra/http/app';
+import supertest from 'supertest';
+import { deleteAll, disconnect } from '../../../../test/database';
+import { createAcademicYear } from '../../../../test/entities/AcademicYearFactory';
+import { createExam } from '../../../../test/entities/ExamFactory';
+import { createQuarter } from '../../../../test/entities/QuarterFactory';
+import { createSubject } from '../../../../test/entities/SubjectFactory';
+import { createUser, authenticateUser } from '../../../../test/entities/UserFactory';
+
+describe('Get exams by user id (e2e)', () => {
+  beforeAll(async () => {
+    deleteAll();
+  });
+
+  afterAll(async () => {
+    disconnect();
+  });
+
+  it('Should return a exam successfully using the user id', async () => {
+    const user = await createUser();
+    const token = await authenticateUser(user);
+    const academicYear = await createAcademicYear(user);
+    const quarter = await createQuarter(academicYear);
+    const subject = await createSubject(quarter);
+    const exam = await createExam(subject, user);
+
+    const response = await supertest(app)
+      .get('/exams/get/user')
+      .set('authorization', 'Bearer ' + token);
+
+    const responseBody = JSON.parse(response.text);
+
+    expect(response.status).toBe(200);
+    expect(responseBody.length).toBe(1);
+  });
+});
---
+++ ufabc_planner-backend/src/modules/exams/controllers/UpdateExamController.spec.ts
@@ -0,0 +1,35 @@
+import { Weekdays, BiweeklyType } from '@prisma/client';
+import { app } from 'infra/http/app';
+import supertest from 'supertest';
+import { deleteAll, disconnect } from '../../../../test/database';
+import { createAcademicYear } from '../../../../test/entities/AcademicYearFactory';
+import { createExam } from '../../../../test/entities/ExamFactory';
+import { createQuarter } from '../../../../test/entities/QuarterFactory';
+import { createSubject } from '../../../../test/entities/SubjectFactory';
+import { createUser, authenticateUser } from '../../../../test/entities/UserFactory';
+
+describe('update exam(e2e)', () => {
+  beforeAll(async () => {
+    deleteAll();
+  });
+
+  afterAll(async () => {
+    disconnect();
+  });
+
+  it('Should update a examsuccessfully', async () => {
+    const user = await createUser();
+    const token = await authenticateUser(user);
+    const academicYear = await createAcademicYear(user);
+    const quarter = await createQuarter(academicYear);
+    const subject = await createSubject(quarter);
+    const exam = await createExam(subject, user);
+
+    const response = await supertest(app)
+      .put('/exams/update/' + exam.id)
+      .set('authorization', 'Bearer ' + token)
+      .send({ dueDate: '2022-08-08', time: '12:00' });
+
+    expect(response.status).toBe(204);
+  });
+});
