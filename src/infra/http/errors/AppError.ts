2ba2637f8537c6768006f97e16dca4d8e3cae8bd
Vinicius Santos <vinicius.santanasantos@yahoo.com.br>
2023-03-19 00:52:23 -0300

    adding error formating and stacktrace

--- src/infra/http/errors/AppError.ts
+++ src/infra/http/errors/AppError.ts
@@ -1,12 +1,44 @@
-export class AppError {
-  public readonly name: string;
-  public readonly message: string;
+export class AppError extends Error {
+  constructor(public message: string, public readonly code: number, public name: string) {
+    super(message);
 
-  public readonly statusCode: number;
+    Object.defineProperty(this, 'message', { enumerable: true });
+    Error.captureStackTrace(this, this.constructor);
+    Object.defineProperty(this, 'stack', { enumerable: true });
+  }
 
-  constructor(message: string, statusCode = 400, name = 'AppError') {
-    this.name = name;
-    this.message = message;
-    this.statusCode = statusCode;
+  toJson(): any {
+    return {
+      ...AppError.format(this),
+      stack: this.stack?.split('\n'),
+    };
+  }
+
+  static format(value: any): any {
+    if (typeof value === 'function' || typeof value === 'number') {
+      return undefined;
+    }
+
+    if (typeof value !== 'object') {
+      return value;
+    }
+
+    if (Array.isArray(value)) {
+      return value.map(AppError.format);
+    }
+
+    if (value instanceof Error) {
+      return Object.getOwnPropertyNames(value).reduce((obj, k) => {
+        obj[k] = AppError.format((value as any)[k]);
+
+        return obj;
+      }, {} as any);
+    }
+
+    return Object.entries(value).reduce((obj, [k, v]) => {
+      obj[k] = AppError.format(v);
+
+      return obj;
+    }, {} as any);
   }
 }
